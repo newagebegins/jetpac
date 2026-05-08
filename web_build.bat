cc0f251f34b91079507f33bbfb074a721d539862
newagebegins <newagebegins@gmail.com>
2026-05-09 08:12:58 +1000

    Put game.wasm into build\web

--- web_build.bat
+++ web_build.bat
@@ -1,2 +1,6 @@
 @echo off
-clang -fno-builtin-memset -Wno-writable-strings -DWASM_BUILD=1 -DGAME_SLOW=1 -DGAME_INTERNAL=1 --target=wasm32 -std=c++11 -nostdlib -O3 -Wl,--no-entry,--export="GameUpdateAndRender",--import-memory -o game.wasm game.cpp
+
+set BuildDir=build\web
+if not exist %BuildDir% mkdir %BuildDir%
+
+clang -fno-builtin-memset -Wno-writable-strings -DWASM_BUILD=1 -DGAME_SLOW=1 -DGAME_INTERNAL=1 --target=wasm32 -std=c++11 -nostdlib -O3 -Wl,--no-entry,--export="GameUpdateAndRender",--import-memory -o %BuildDir%\game.wasm game.cpp
