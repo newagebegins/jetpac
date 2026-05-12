98e5252c14e76e9ad27756faeaa8c724304eafee
newagebegins <newagebegins@gmail.com>
2026-05-12 12:08:27 +1000

    Use -mbulk-memory instead of -fno-builtin-memset

--- build.bat
+++ build.bat
@@ -18,4 +18,4 @@ cl %CommonCompilerFlags% ..\win32_game.cpp user32.lib gdi32.lib winmm.lib /link 
 
 popd
 
-clang -fno-builtin-memset -Wno-writable-strings -DWASM_BUILD=1 -DGAME_SLOW=1 -DGAME_INTERNAL=1 --target=wasm32 -std=c++11 -nostdlib -O3 -Wl,--no-entry,--export="GameUpdateAndRender",--import-memory -o game.wasm game.cpp
+clang -mbulk-memory -Wno-writable-strings -DWASM_BUILD=1 -DGAME_SLOW=1 -DGAME_INTERNAL=1 --target=wasm32 -std=c++11 -nostdlib -O3 -Wl,--no-entry,--export="GameUpdateAndRender",--import-memory -o game.wasm game.cpp
